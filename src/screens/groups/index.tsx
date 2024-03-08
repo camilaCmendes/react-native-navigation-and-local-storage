65fddb0cf393756c514884ccdd493271a1033749
Camila <mendemmcamila@gmail.com>
2024-03-08 14:18:11 -0300

    feat: Implement groups listing feature

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,13 +1,28 @@
 import { Header, HighLight } from "@/components";
 import { View } from "react-native";
-import { GroupCard } from "./components";
+import { GroupCard, ListEmpty } from "./components";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import * as S from "./styles";
 
 export const Groups: React.FC = () => {
+  const [groups, setGroups] = useState<string[]>([]);
   return (
-    <View>
+    <S.Container>
       <Header />
       <HighLight title="Turmas" subtitle="Jogue com suas turmas" />
-      <GroupCard title="Galera do Ignite" onPress={() => null} />
-    </View>
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => null} />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        }
+      />
+    </S.Container>
   );
 };
