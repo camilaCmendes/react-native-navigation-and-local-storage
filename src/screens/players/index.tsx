3bcd63c3912ce0d389e774446227565b44663446
Camila <mendemmcamila@gmail.com>
2024-03-10 13:04:59 -0300

    feat: update Players screen with new Filter component

--- src/screens/players/index.tsx
+++ src/screens/players/index.tsx
@@ -1,7 +1,12 @@
 import { ButtonIcon, Header, HighLight, Input } from "@/components";
 import * as S from "./styles";
+import { Filter } from "./components/filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 export const Players: React.FC = () => {
+  const [team, setTeam] = useState("time a");
+  const [players, setPlayers] = useState([]);
   return (
     <>
       <Header showBackButton />
@@ -13,6 +18,21 @@ export const Players: React.FC = () => {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="Plus" onPress={() => null} color={"green700"} />
       </S.Form>
+      <S.HeaderList>
+        <FlatList
+          data={["time a", "time b"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <S.NumbersOfPlayers>{players.length}</S.NumbersOfPlayers>
+      </S.HeaderList>
     </>
   );
 };
--- src/screens/players/styles.ts
+++ src/screens/players/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Form = styled.View`
   width: 100%;
@@ -6,4 +6,18 @@ export const Form = styled.View`
   flex-direction: row;
   justify-content: center;
   border-radius: 6px;
+`;
+
+export const HeaderList = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  margin: 32px 0 12px;
+`;
+
+export const NumbersOfPlayers = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray200};
+    font-size: ${theme.fontSizes.small}px;
+  `}
 `;
