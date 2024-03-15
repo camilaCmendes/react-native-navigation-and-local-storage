bfd048de422a4cc14f6c32a045ad0f429ed42e80
Camila <mendemmcamila@gmail.com>
2024-03-15 11:56:42 -0300

    feat: Detect device language for app usage

--- i18n.ts
+++ i18n.ts
@@ -1,17 +1,23 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import { pt } from "./src/translations/pt";
+import { NativeModules, Platform } from "react-native";
 import { en } from "./src/translations/en";
+import { pt } from "./src/translations/pt";
 
 const resources = {
   en,
   pt,
 };
 
+const deviceLanguage =
+  Platform.OS === "ios"
+    ? NativeModules.SettingsManager.settings.AppleLocale // iOS
+    : NativeModules.I18nManager.localeIdentifier; // Android
+
 i18n.use(initReactI18next).init({
   compatibilityJSON: "v3",
   resources,
-  lng: "pt", // Idioma padrão
+  lng: deviceLanguage || "en", // Idioma padrão
   interpolation: {
     escapeValue: false,
   },
