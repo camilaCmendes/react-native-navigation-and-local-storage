0b772a19db73e6c95fa47f1a0aedb0ee76098b02
Camila <mendemmcamila@gmail.com>
2024-03-14 21:49:22 -0300

    feat: add logic to players screen

--- src/screens/players/index.tsx
+++ src/screens/players/index.tsx
@@ -7,12 +7,16 @@ import {
   ListEmpty,
 } from "@/components";
 import { useRoute } from "@react-navigation/native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { FlatList } from "react-native";
+import { Alert, FlatList } from "react-native";
 import { Filter } from "./components/filter";
 import { PlayerCard } from "./components/playerCard";
 import * as S from "./styles";
+import { AppError } from "@/utils/AppError";
+import { playerAddByGroup } from "@/storage/player/playerAddByGroup";
+import { playersGetByGroupAndTeam } from "@/storage/player/playersGetByGroupAndTeam";
+import { PlayerStorageDTO } from "@/storage/player/playerStorageDTO";
 
 type RouteParams = {
   group: string;
@@ -24,7 +28,51 @@ export const Players: React.FC = () => {
 
   const { group } = route.params as RouteParams;
   const [team, setTeam] = useState(t("players_teamA"));
-  const [players, setPlayers] = useState(["Camila", "Leonardo"]);
+  const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
+  const [newPlayerName, setNewPlayerName] = useState("");
+
+  const handleAddPlayer = async () => {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert(
+        "Nova pessoa",
+        "Informe o nome da pessoa para adicionar."
+      );
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    };
+
+    try {
+      await playerAddByGroup(newPlayer, group);
+      fetchPlayersByTeam();
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Nova pessoa", error.message);
+      } else {
+        console.log(error);
+        Alert.alert("Nova pessoa", "Não foi possível adicionar.");
+      }
+    }
+  };
+
+  const fetchPlayersByTeam = async () => {
+    try {
+      const players = await playersGetByGroupAndTeam(group, team);
+      setPlayers(players);
+    } catch (error) {
+      console.log(error);
+      Alert.alert(
+        "Pessoas",
+        "Não foi possível carregar as pessoas do time selecionado"
+      );
+    }
+  };
+
+  useEffect(() => {
+    fetchPlayersByTeam();
+  }, [team]);
 
   return (
     <S.Container>
@@ -32,10 +80,15 @@ export const Players: React.FC = () => {
       <HighLight title={group} subtitle={t("players_subtitle")} />
       <S.Form>
         <Input
+          onChangeText={setNewPlayerName}
           placeholder={t("players_inputPlaceholder")}
           autoCorrect={false}
         />
-        <ButtonIcon icon={"Plus"} onPress={() => null} color={"green700"} />
+        <ButtonIcon
+          icon={"Plus"}
+          onPress={handleAddPlayer}
+          color={"green700"}
+        />
       </S.Form>
       <S.HeaderList>
         <FlatList
@@ -55,9 +108,9 @@ export const Players: React.FC = () => {
 
       <FlatList
         data={players}
-        keyExtractor={(item) => item}
+        keyExtractor={(item) => item.name}
         renderItem={({ item }) => (
-          <PlayerCard name={item} onRemove={() => null} />
+          <PlayerCard name={item.name} onRemove={() => null} />
         )}
         ListEmptyComponent={() => (
           <ListEmpty message={t("players_emptyListMessage")} />
--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,3 +1,4 @@
 const GROUP_COLLECTION = "@react-native-navigation-and-local-storage:groups";
+const PLAYER_COLLECTION = "@react-native-navigation-and-local-storage:players";
 
-export { GROUP_COLLECTION };
+export { GROUP_COLLECTION, PLAYER_COLLECTION };
